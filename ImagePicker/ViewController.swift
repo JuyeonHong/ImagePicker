e95578f13a5325f7758d2d52480855395329851d
hongjuyeon <coke1317@naver.com>
2019-09-16 10:26:05 +0900

    앨범, 카메라 여는 동작 추가 - UIImagePickerController tkdyd

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -11,26 +11,41 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       imagePicker.delegate = self
     }
-
+    
+    func openAlbum(){
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func openCamera(){
+        imagePicker.sourceType = .camera
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func addImage(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "", message: "사진 설정", preferredStyle: .actionSheet)
 
         alert.addAction(UIAlertAction(title: "앨범에서 사진 선택", style: .default, handler: { (_) in
             print("앨범 열기")
+            self.openAlbum()
         }))
         alert.addAction(UIAlertAction(title: "카메라로 사진 촬영", style: .default, handler: { (_) in
             print("카메라로 사진 촬영")
+            self.openCamera()
         }))
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
     }
-    
-    
+
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+}
